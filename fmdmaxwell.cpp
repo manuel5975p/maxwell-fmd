83c4a6813d34c73b5e6e18dc8e2a3abe8a2029e4
Your Name <you@example.com>
2020-12-12 15:43:00 +0100

    working + progress

--- fmdmaxwell.cpp
+++ fmdmaxwell.cpp
@@ -4,12 +4,14 @@
 #include <utility>
 #include <tuple>
 #include <any>
-#include <sstream>
 #include <SFML/Window.hpp>
+#include <sstream>
 #include <SFML/Graphics.hpp>
 constexpr double c = 299792458;
 constexpr double mu_0 = 4 * M_PI * 1e-7;
 constexpr double eps_0 = 1.0 / (mu_0 * c * c);
+constexpr size_t border_thickness = 64;
+constexpr double border_intensity = 1.0 / 16.0;
 template<typename _scalar>
 struct yee_grid2d{
     using scalar = _scalar;
@@ -26,9 +28,10 @@ struct yee_grid2d{
     array j_source;
     array m_source;
     size_t m, n;
+    array multmask;
     yee_grid2d(size_t _m, size_t _n) : 
     e_x(_m, _n - 1), e_y(_m - 1, _n), h_z(_m - 1, _n - 1), sigma_e(_m, _n), sigma_h(_m - 1, _n - 1), mu(_m - 1, _n - 1),
-    eps(_m, _n),j_source(_m - 1, _n - 1), m_source(_m - 1, _n - 1), m(_m), n(_n)
+    eps(_m, _n),j_source(_m - 1, _n - 1), m_source(_m - 1, _n - 1), m(_m), n(_n), multmask(_m, _n)
     {
         assert(_m > 1);
         assert(_n > 1);
@@ -41,12 +44,27 @@ struct yee_grid2d{
         sigma_h.fill(0);
         j_source.setZero();
         m_source.setZero();
+        multmask.setConstant(std::exp(-border_intensity));
+        for(size_t i = 0;i < border_thickness;i++){
+            if(i == border_thickness - 1){
+                multmask.block(i,i, m - 2 * i, n - 2 * i) = 1;
+            }
+            else{
+                multmask.block(i,i, m - 2 * i, n - 2 * i) *= std::exp(border_intensity / border_thickness);
+            }
+        }
     }
     scalar rot_E_z(size_t i, size_t j)const{
         return (e_y(i, j + 1) - e_y(i, j)) - (e_x(i + 1, j) - e_x(i, j));
     }
     vector2 rot_H_xy(size_t i, size_t j)const{
         return vector2(h_z(i, j) - h_z(i - 1, j), -(h_z(i, j) - h_z(i, j - 1)));
+    }
+    size_t realm()const{
+        return m - 2 * border_thickness;
+    }
+    size_t realn()const{
+        return n - 2 * border_thickness;
     }
     yee_grid2d<scalar>& operator+=(const yee_grid2d<scalar>& o){
         e_x.noalias() += o.e_x;
@@ -101,7 +119,7 @@ struct yee_grid2d{
         e_x_deriv -= (sigma_e.block(0, 1, m, n - 1) + sigma_e.block(0, 0, m, n - 1)) * 0.5 * e_x;
         e_y_deriv -= (sigma_e.block(1, 0, m - 1, n) + sigma_e.block(0, 0, m - 1, n)) * 0.5 * e_y;
         h_z_deriv -= m_source;
-        h_z_deriv /= mu;
+        h_z_deriv /= (mu);
         e_x_deriv /= ((eps.block(0, 1, m, n - 1) + eps.block(0, 0, m, n - 1)) * 0.5);
         e_y_deriv /= ((eps.block(1, 0, m - 1, n) + eps.block(0, 0, m - 1, n)) * 0.5);
         
@@ -148,6 +166,8 @@ yee_grid2d<scalar> timestep(const yee_grid2d<scalar>& grid, double h){
     retgrid.e_x += std::get<1>(kappa3) * h / 6.0;
     retgrid.e_y += std::get<2>(kappa3) * h / 6.0;
     
+    retgrid.h_z *= retgrid.multmask.block(0,0,retgrid.m - 1, retgrid.n - 1);
+
     return retgrid;
 }
 using scalar = float;
@@ -178,32 +198,35 @@ std::string tu_string(float x){
 }
 int main(){
     using namespace Eigen;
-    
-    yee_grid2d<scalar> grid(256, 256);
-    yee_grid2d<scalar> dual_grid(256, 256);
+    bool paused = true;
+    constexpr size_t M = 256 + border_thickness * 2;
+    constexpr size_t N = 256 + border_thickness * 2;
+    yee_grid2d<scalar> grid      (M, N);
+    yee_grid2d<scalar> dual_grid (M, N);
     dual_grid.eps = grid.eps;
     dual_grid.mu = grid.mu;
     dual_grid.sigma_e = grid.sigma_e;
     dual_grid.sigma_h = grid.sigma_h;
     using array = yee_grid2d<scalar>::array;
-    for(size_t i = 0;i < grid.m - 1;i++){
+    for(int i = -25;i < 25;i++){
         for(int j = -25;j < 25;j++){
-            grid.h_z(i, j + 25) = std::exp(-j * j / 60.0);
+            grid.h_z(i + 128 + border_thickness, j + 50 + border_thickness) = 3 * std::exp(-(i * i + j * j) / 120.0);
         }
     }
     grid.sigma_e.fill(0);
     //grid.sigma_e.block(0, 70, grid.sigma_e.rows(), 3) = 1;
-    Vector2f lenspoint1(0.50,0.60);
-    Vector2f lenspoint2(0.50,0.90);
+    Vector2f lenspoint1(0.57, 0.60);
+    Vector2f lenspoint2(0.57, 0.90);
     array dmu(grid.m - 1, grid.n - 1);
     dmu.fill(0);
     scalar F = 0;
     scalar dFdPhi = 0;
     for(size_t i = 0;i < grid.m - 2;i++){
         for(size_t j = 0;j < grid.n - 2;j++){
-            Vector2f vec(float(i) / grid.m, float(j) / grid.n);
+            Vector2f vec(float(i - border_thickness) / grid.realm(), float(j - border_thickness) / grid.realn());
             if((vec - lenspoint1).cwiseAbs().norm() < 0.2 && (vec - lenspoint2).cwiseAbs().norm() < 0.2){
-                grid.mu(i, j) *= 2;
+                grid.mu(i, j) *=  1.1;
+                grid.eps(i, j) *= 1;
                 dmu(i, j) = mu_0;
             }
             //if((vec - lenspoint1).cwiseAbs().maxCoeff() < 0.3){
@@ -216,9 +239,9 @@ int main(){
     using array = decltype(grid)::array;
     sf::Texture tex;
     double time = 0;
-    tex.create(grid.m - 1, grid.n - 1);
+    tex.create(grid.m - 1 - 2 * border_thickness, grid.n - 1 - 2 * border_thickness);
     sf::Sprite schprit(tex);
-    schprit.setScale(window.getSize().x / (grid.m - 1.0),window.getSize().y / (grid.n - 1.0));
+    schprit.setScale(window.getSize().x / (grid.m - 1.0 - 2.0 * border_thickness),window.getSize().y / (grid.n - 1.0 - 2.0 * border_thickness));
     window.setVerticalSyncEnabled(0);
     window.setFramerateLimit(0);
     array elong = 
@@ -228,7 +251,7 @@ int main(){
     auto elongmax = elong.maxCoeff() * 10000;
     auto sigmamax = grid.sigma_e.maxCoeff() + 0.001;
     sf::Font font;
-    if (!font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf"))
+    if (!font.loadFromFile("../Arial.ttf"))
     {
         std::terminate();
     }
@@ -242,11 +265,15 @@ int main(){
                 if(event.key.code == sf::Keyboard::Escape){
                     window.close();
                 }
+                if(event.key.code == sf::Keyboard::P){
+                    paused = !paused;
+                }
             }
         }
         window.clear();
+        if(!paused)
         for(size_t i = 0;i < 1;i++){
-            const decltype(grid)::scalar tstp = 0.0005 / c;
+            const decltype(grid)::scalar tstp = 0.001 / c;
             auto grid2 = timestep<decltype(grid)::scalar>(grid, tstp);
             dual_grid.m_source = dmu * ((grid2.h_z - grid.h_z) / tstp);
             //std::cout << "msource: " << (grid2.h_z - grid.h_z).maxCoeff() << "\n";
@@ -255,9 +282,9 @@ int main(){
             grid = std::move(grid2);
             time += tstp;
             vector3 E_at_point(0,0,0);
-            vector3 H_at_point(0, 0, grid.h_z(grid.m / 2, 7 * grid.n / 8));
+            vector3 H_at_point(0, 0, grid.h_z(grid.realm() / 2, 7 * grid.realn() / 8));
             vector3 gE_at_point(0,0,0);
-            vector3 gH_at_point(0, 0, dual_grid.h_z(dual_grid.m / 2, 7 * dual_grid.n / 8));
+            vector3 gH_at_point(0, 0, dual_grid.h_z(dual_grid.realm() / 2, 7 * dual_grid.realn() / 8));
             vector6 gEandH;
             gEandH.head(3) = gE_at_point;
             gEandH.tail(3) = gH_at_point;
@@ -267,29 +294,33 @@ int main(){
             
         }
         //std::terminate();
-        std::vector<unsigned char> texdata((grid.m - 1) * (grid.n - 1) * 4);
+        std::vector<unsigned char> texdata((grid.m - 1 - border_thickness * 2) * (grid.n - 1 - border_thickness * 2) * 4);
+        //std::cout << texdata.size() << std::endl;
         elong = 
           ((grid.e_x.block(1, 0, grid.m - 1, grid.n - 1) + grid.e_x.block(0, 0, grid.m - 1, grid.n - 1)) * 0.5).square() 
         + ((grid.e_y.block(0, 1, grid.m - 1, grid.n - 1) + grid.e_y.block(0, 0, grid.m - 1, grid.n - 1)) * 0.5).square()
         + (grid.h_z).square();
-        
-        for(size_t i = 0;i < grid.m - 1;i++){
-            for(size_t j = 0;j < grid.n - 1;j++){
-                if(i == grid.m / 2 && j == 7 * grid.n / 8){
-                    texdata[(i * (grid.n - 1) + j) * 4] =     255;
-                    texdata[(i * (grid.n - 1) + j) * 4 + 1] = 0;
-                    texdata[(i * (grid.n - 1) + j) * 4 + 2] = 0;
-                    texdata[(i * (grid.n - 1) + j) * 4 + 3] = 255;
+        auto blockexp_hz = grid.h_z.block(border_thickness, border_thickness, grid.m - 1 - border_thickness * 2, grid.n - 1 - border_thickness * 2);
+        auto blockexp_dghz = dual_grid.h_z.block(border_thickness, border_thickness, grid.m - 1 - border_thickness * 2, grid.n - 1 - border_thickness * 2);
+        auto blockexp_mu = grid.mu.block(border_thickness, border_thickness, grid.m - 1 - border_thickness * 2, grid.n - 1 - border_thickness * 2);
+        for(size_t i = 0;i < grid.m - 1 - border_thickness * 2;i++){
+            for(size_t j = 0;j < grid.n - 1 - border_thickness * 2;j++){
+                if(i == grid.realm() / 2 && j == 7 * grid.realn() / 8){
+                    texdata[(i * (grid.realn() - 1) + j) * 4] =     255;
+                    texdata[(i * (grid.realn() - 1) + j) * 4 + 1] = 0;
+                    texdata[(i * (grid.realn() - 1) + j) * 4 + 2] = 0;
+                    texdata[(i * (grid.realn() - 1) + j) * 4 + 3] = 255;
                 }
                 else{
-                    texdata[(i * (grid.n - 1) + j) * 4] = fcolor(std::abs(grid.h_z(i,j)));
-                    texdata[(i * (grid.n - 1) + j) * 4 + 1] = fcolor(relu(dual_grid.h_z(i,j)));//fcolor(std::abs(grid.e_x(i,j)));
-                    texdata[(i * (grid.n - 1) + j) * 4 + 2] = fcolor(grid.mu(i, j) / mu_0 / 3);//fcolor(std::abs(grid.e_y(i,j)));;
-                    texdata[(i * (grid.n - 1) + j) * 4 + 3] = 255;
+                    texdata[(i * (grid.n - 1 - 2 * border_thickness) + j) * 4] = fcolor(std::abs(blockexp_hz(i,j)));
+                    texdata[(i * (grid.n - 1 - 2 * border_thickness) + j) * 4 + 1] = fcolor(relu(blockexp_dghz(i,j)));//fcolor(std::abs(grid.e_x(i,j)));
+                    texdata[(i * (grid.n - 1 - 2 * border_thickness) + j) * 4 + 2] = fcolor(std::pow(blockexp_mu(i, j) / mu_0, 10) / 3);//fcolor(std::abs(grid.e_y(i,j)));;
+                    texdata[(i * (grid.n - 1 - 2 * border_thickness) + j) * 4 + 3] = 255;
                 }
             }
         }
         tex.update(texdata.data());
+        tex.setSmooth(true);
         window.draw(schprit);
         std::stringstream sstr;
         sstr.precision(3);
@@ -302,7 +333,7 @@ int main(){
         sf::Text bigF("F = " + tu_string(F), font);
         sf::Text gradF("dF/dphi = " + tu_string(dFdPhi), font);
         sf::Text info_H("Red = magnetic magnitude", font);
-        sf::Text info_grad("Yellow (actually only green) = positive magnetic gradient", font);
+        sf::Text info_grad("Green = positive magnetic gradient", font);
         text.setPosition(sf::Vector2f (0,0));
         bigF.setPosition(sf::Vector2f (0,50));
         gradF.setPosition(sf::Vector2f(0,100));
